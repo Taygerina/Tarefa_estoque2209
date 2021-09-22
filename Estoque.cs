9a05a218135179ceb9f90be2f4ad7ed5c11cef39
Talita <talitamarcelo15@gmail.com>
2021-09-22 13:12:45 -0300

    Adicionar arquivos de projeto.

---
+++ Estoque.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estoque
+{
+    public class produto
+    {
+        static void Main(string[] args)
+        {
+            // interaçao com usuario
+            Console.WriteLine("Entre com dados do produto: ");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            // declarando classe de produtos
+            Produto p = new Produto(nome, preco, quantidade);
+
+            // interaçao com usuario
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProduto(qte);
+
+            // resultado 
+            Console.WriteLine("Dados do atualizados: " + p);
+
+            // resultado
+            Console.WriteLine("Digite o número de produtos a serem removidos do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            // resultado final
+            Console.WriteLine("Dados do atualizados: " + p);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Atualizaçãoestoque
+{
+
+    class Produto
+    {
+        //declaração de variaveis do produto
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        //construtor personalizado
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        //utilizando do GET e SET
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetName(string nome)
+        {
+            _nome = nome;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+
+        public double ValorTotalEStoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            _quantidade -= quantidade;
+        }
+
+        // resoltado final
+
+        public override string ToString()
+        {
+            return _nome + ", R$ " + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade + " unidades, Total: R$ " + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+    
+
+
+}
